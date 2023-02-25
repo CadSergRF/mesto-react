9389ee6b5e18958be2d22822e54dfc8e007c72e9
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-02-25 20:09:32 +0700

    feat: модальные окна

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Footer from './Footer';
 import Header from './Header'
 import Main from './Main'
 import { useState } from 'react';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
 
@@ -38,60 +39,59 @@ function App() {
         onEditProfile={handleEditProfileClick}
         onAddPlace={handleAddPlaceClick}
         onEditAvatar={handleEditAvatarClick}
-        onClose={closeAllPopups}
       />
 
       <Footer />
 
-      {/* <!-- Попап "Редактирование профиля"--> */}
-      <div className={`popup ${isEditProfilePopupOpen && "popup_opened"}`} id="edit-profile">
-        <div className="popup__container">
-          <button 
-          className="popup__close-btn" 
-          type="button" 
-          aria-label="Кнопка закрытия окна редактирования профиля" 
-          onClick={closeAllPopups}></button>
-          <h3 className="popup__title">Редактировать профиль</h3>
-          <form className="popup__form" name="editProfile">
-            <input className="popup__input popup__input_edit_name" type="text" placeholder="Ваше имя/ник" name="editProfileName"
-              minLength="2" maxLength="40" required />
-            <span className="popup__input-error editProfileName-error"></span>
-            <input className="popup__input popup__input_edit_job" type="text" placeholder="Ваша должность" name="editProFileJob"
-              minLength="2" maxLength="200" required />
-            <span className="popup__input-error editProFileJob-error"></span>
-            <button className="popup__save-btn" type="submit" aria-label="Кнопка сохранить изменения">Сохранить</button>
-          </form>
-        </div>
-      </div>
-      {/* <!-- Попап "Добавления контента"--> */}
-      <div className={`popup ${isAddPlacePopupOpen && "popup_opened"}`} id="add-place">
-        <div className="popup__container">
-          <button className="popup__close-btn" type="button" aria-label="Кнопка закрытия окна добавления контента" onClick={closeAllPopups}></button>
-          <h3 className="popup__title">Новое место</h3>
-          <form className="popup__form" name="addPlace">
-            <input className="popup__input popup__input_place_name" type="text" placeholder="Название" name="name" minLength="2"
-              maxLength="30" required />
-            <span className="popup__input-error name-error"></span>
-            <input className="popup__input popup__input_place_link" type="URL" placeholder="Ссылка на картинку" name="link"
-              required />
-            <span className="popup__input-error link-error"></span>
-            <button className="popup__save-btn" type="submit" aria-label="Кнопка сохранить изменения">Создать</button>
-          </form>
-        </div>
-      </div>
-      {/* <!-- Попап "Изменение аватара"--> */}
-      <div className={`popup ${isEditAvatarPopupOpen && "popup_opened"}`} id="edit-avatar">
-        <div className="popup__container">
-          <button className="popup__close-btn" type="button" aria-label="Кнопка закрытия окна добавления контента" onClick={closeAllPopups}></button>
-          <h3 className="popup__title">Обновить аватар</h3>
-          <form className="popup__form" name="editAvatar">
-            <input className="popup__input popup__input_place_link" type="URL" placeholder="Ссылка на картинку" name="link"
-              required />
-            <span className="popup__input-error link-error"></span>
-            <button className="popup__save-btn" type="submit" aria-label="Кнопка сохранить изменения">Сохранить</button>
-          </form>
-        </div>
-      </div>
+
+      {/* Модалка редактирование профиля */}
+      <PopupWithForm
+        name="editProfile"
+        title="Редактировать профиль"
+        btnSubmitText="Сохранить"
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+      >
+        <input className="popup__input popup__input_edit_name" type="text" placeholder="Ваше имя/ник" name="editProfileName"
+          minLength="2" maxLength="40" required />
+        <span className="popup__input-error editProfileName-error"></span>
+        <input className="popup__input popup__input_edit_job" type="text" placeholder="Ваша должность" name="editProFileJob"
+          minLength="2" maxLength="200" required />
+        <span className="popup__input-error editProFileJob-error"></span>
+      </PopupWithForm>
+
+      {/* Модалка Добавления контента */}
+      <PopupWithForm
+        name="addPlace"
+        title="Новое место"
+        btnSubmitText="Создать"
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}
+      >
+        <input className="popup__input popup__input_place_name" type="text" placeholder="Название" name="name" minLength="2"
+          maxLength="30" required />
+        <span className="popup__input-error name-error"></span>
+        <input className="popup__input popup__input_place_link" type="URL" placeholder="Ссылка на картинку" name="link"
+          required />
+        <span className="popup__input-error link-error"></span>
+      </PopupWithForm>
+
+    {/* Модалка Изменение аватара */}
+    <PopupWithForm
+        name="editAvatar"
+        title="Обновить аватар"
+        btnSubmitText="Сохранить"
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+      >
+        <input className="popup__input popup__input_place_name" type="text" placeholder="Название" name="name" minLength="2"
+          maxLength="30" required />
+        <span className="popup__input-error name-error"></span>
+        <input className="popup__input popup__input_place_link" type="URL" placeholder="Ссылка на картинку" name="link"
+          required />
+        <span className="popup__input-error link-error"></span>
+      </PopupWithForm>
+     
       {/* <!-- Попап "Подтверждение действия" --> */}
       <div className="popup" id="confirm-popup">
         <div className="popup__container">
