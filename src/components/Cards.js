d0d7084448c0b1527e46c7200229ba10c6ee8934
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-02-25 21:40:49 +0700

    feat: компонент Cards

---
+++ src/components/Cards.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Cards = ({ cardData }) => {
+  return (
+    <li className="places__item">
+      <article className="place">
+        <img className="place__image" src={cardData.link} alt={cardData.name} />
+        <button className="place__delete" type="button" aria-label="Кнопка удаления place"></button>
+        <div className="place__info">
+          <h2 className="place__title">{cardData.name}</h2>
+          <div>
+            <button className="place__like" type="button" aria-label="Кнопка лайк"></button>
+            <p className="place__num-of-likes">{cardData.likes.length}</p>
+          </div>
+        </div>
+      </article>
+    </li>
+  )
+}
+
+export default Cards
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import api from '../utils/Api';
+import Cards from './Cards';
 
 const Main = (props) => {
 
   const [userName, setUserName] = useState("");
   const [userDescription, setUserDescription] = useState("");
   const [userAvatar, setUserAvatar] = useState("");
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
     api
-    .getUserInfo()
-    .then((userData) => {
-      setUserName(userData.name);
-      setUserDescription(userData.about);
-      setUserAvatar(userData.avatar);
-      console.log(userData);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .getUserInfo()
+      .then((userData) => {
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+        setUserAvatar(userData.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    api
+      .getInitialCards()
+      .then((cardsData) => {
+        setCards(cardsData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [])
-  
+
 
   return (
     <main className="root__container">
       {/* <!-- профиль пользователя --> */}
       <section className="user-profile root__user-profile">
         <div className="user-profile__avatar-wrapper" onClick={props.onEditAvatar}>
-          <img className="user-profile__photo" alt="Аватар" src={userAvatar}/>
+          <img className="user-profile__photo" alt="Аватар" src={userAvatar} />
         </div>
         <div className="user-profile__info-wrapper">
           <h1 className="user-profile__name">{userName}</h1>
@@ -50,6 +60,14 @@ const Main = (props) => {
       {/* <!-- Секция контента --> */}
       <section className="places" aria-label="Фотографии">
         <ul className="places__list">
+          {
+            cards.map((cardData) => (
+              <Cards 
+              cardData={cardData}
+              key={cardData._id}
+              />
+            ))
+          }
         </ul>
       </section>
 
